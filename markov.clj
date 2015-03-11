85ce402ddbb30a28123fdfe41aa030fa46a72efd
Shivam Mistry <admin@shivam-mistry.co.uk>
2015-03-11 20:36:38 +0000

    Formatting

--- markov.clj
+++ markov.clj
@@ -1,6 +1,6 @@
 (require '[clojure.string :as str])
 
-(def n-g 3)
+(def n-g 2)
 
 (defn read-log [file]
   (let [matcher (map (partial re-matcher #".*?<\s*(.+?)>\s+?(.+)") (str/split-lines (slurp file)))] 
@@ -37,15 +37,13 @@
   (let [word (rand-nth (get ngrams head)) l (vec head)]
     (if (nil? word) nil
       (let [chain (generate-sentence ngrams (conj (vec (nthrest l 1)) word))]
-        (conj (vec (take 1 head)) (if (nil? chain) (conj nil word (drop 1 l))  chain))
-        )
-      )
+        (conj (vec (take 1 head)) (if (nil? chain) (conj nil word (drop 1 l)) chain))))
     )
-
   )
 
 (defn get-user 
   ([user m] (flatten (let [col (get m user)] (generate-sentence col (rand-nth (keys col))))))
   ([user m first-word] (flatten (generate-sentence (get m user) first-word)))
   )
-(println (get-user (second *command-line-args*) (parse-logs (read-log (first *command-line-args*)))))
+;
+;(println (get-user (second *command-line-args*) (parse-logs (read-log (first *command-line-args*)))))
